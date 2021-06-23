879beeb9e0b7d761e7e494f1253a916763ba2c04
Vishal45-coder <vishalraavi1999@gmail.com>
2021-06-23 22:14:51 +0530

    Delete comments

--- src/Getter_setter.java
+++ src/Getter_setter.java
@@ -2,7 +2,6 @@
 public class Getter_setter {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		ge h1=new ge();
         h1.setA(10);
 		System.out.println(h1.getA());
