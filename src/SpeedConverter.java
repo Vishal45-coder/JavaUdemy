609fa6231671d224550359e85b4eb0aac0ff68df
Vishal45-coder <vishalraavi1999@gmail.com>
2021-06-25 12:47:26 +0530

    Methods challenge1

---
+++ src/SpeedConverter.java
@@ -0,0 +1,25 @@
+public class SpeedConverter {
+    // write your code here
+    public static void main(String[] args){
+        System.out.println(toMilesPerHour(1.5));
+        printConversion(1.5);
+    }
+    public static long toMilesPerHour(double kilometersPerHour){
+        if(kilometersPerHour<0){
+            return -1;
+        }
+        else{
+           
+            return Math.round(kilometersPerHour/1.609);
+    }
+    }
+    public static void printConversion(double kilometersPerHour){
+        if(kilometersPerHour<0){
+            System.out.println("Invalid");
+        }
+        else{
+            System.out.println(kilometersPerHour+" Km/h = "+toMilesPerHour(kilometersPerHour)+" mi/h");
+    
+        }  
+    }
+}
