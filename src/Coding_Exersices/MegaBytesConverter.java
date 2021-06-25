0674c12d6720f7c70ab50f0837ba4251335968fb
Vishal45-coder <vishalraavi1999@gmail.com>
2021-06-25 18:15:09 +0530

    Created Coding_Exersices package and add 2 files

---
+++ src/Coding_Exersices/MegaBytesConverter.java
@@ -0,0 +1,20 @@
+package Coding_Exersices;
+
+public class MegaBytesConverter {
+    public static void main(String[] args){
+
+        printMegaBytesAndKiloBytes(2500);
+    }
+    public static void printMegaBytesAndKiloBytes(int kb){
+        if(kb<0){
+            System.out.println("Invalid Value");
+        }
+        else{
+            int div=kb/1024;
+            int div1=kb%1024;
+            System.out.println(kb+" KB = "+div+" MB and "+div1+" KB");
+        }
+
+    }
+}
+
