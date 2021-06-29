73eeb709d306fb7bff3880073980367160c18b93
Vishal45-coder <vishalraavi1999@gmail.com>
2021-06-29 16:07:53 +0530

    Created loops challenge

---
+++ src/Coding_Exersices/Loops_2.java
@@ -0,0 +1,39 @@
+package Coding_Exersices;
+import java.util.Scanner;
+
+public class Loops_2{
+    public static void main(String []args){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++)
+        {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+
+            int num1=0;
+            int evennum=2;
+            int even=0;
+            int n1=0;
+            while(n1 < n){
+                if(n1==0){
+                    num1=a+b;
+                    System.out.print(num1+" ");
+                    n1++;
+                }
+                else{
+                    even=evennum*b;
+                    num1=num1+even;
+                    System.out.print(num1+" ");
+                    evennum*=2;
+                    n1++;
+                }
+         }
+
+       
+
+        }
+        in.close();
+    }
+
+}
