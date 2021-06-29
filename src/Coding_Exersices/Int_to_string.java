5af09c83016f2f7f2444a6faef54670b353ece52
Vishal45-coder <vishalraavi1999@gmail.com>
2021-06-29 15:38:21 +0530

    Created  Int_to_string

---
+++ src/Coding_Exersices/Int_to_string.java
@@ -0,0 +1,16 @@
+package Coding_Exersices;
+
+public class Int_to_string {
+    public static void main(String[] args){
+        int i=100;
+        String s=""+i+""; //Brute force change into string
+        System.out.println(s);
+
+        String s1=String.valueOf(i); //Function to change into  string
+        System.out.println(s1);
+
+        String s2=Integer.toString(i); //Another type of function to change into string
+        System.out.println(s2);
+    }
+    
+}
