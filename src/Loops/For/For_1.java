da1ba0941a0620ff3834dcf8bf8227ac4af070b3
Vishal45-coder <vishalraavi1999@gmail.com>
2021-06-30 11:29:51 +0530

    Created For_1

---
+++ src/Loops/For/For_1.java
@@ -0,0 +1,21 @@
+package Loops.For;
+
+public class For_1 {
+    public static void main(String[] args){
+
+        int n=10;
+        System.out.println("to print "+n+" numbers continuosly");
+
+        /**
+         * for loop structure
+         * for(intialization;condition;incrementation or decrementation){
+         *   write for loop code here
+         * }
+         */
+        for(int i=0;i<n;i++)
+        {
+            System.out.println(i);
+        }
+    }
+    
+}
