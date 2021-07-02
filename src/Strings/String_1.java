d7b4c1ceb4c44e2a9ca9b97cc2e443e09bcb7a0f
Vishal45-coder <vishalraavi1999@gmail.com>
2021-07-02 22:03:46 +0530

    Created Strings

---
+++ src/Strings/String_1.java
@@ -0,0 +1,11 @@
+package Strings;
+
+public class String_1 {
+    public static void main(String[] args){
+        String value="2018"; 
+        String value1="hi this is a String";
+        System.out.println(value);
+        System.out.println(value1);
+    }
+    
+}
