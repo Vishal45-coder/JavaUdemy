87e33a91e8a8d14227f668051d6558d55c49291d
Vishal45-coder <vishalraavi1999@gmail.com>
2021-07-03 11:14:58 +0530

    Conversion of String to double

--- src/Strings/String_1.java
+++ src/Strings/String_1.java
@@ -6,6 +6,19 @@ public class String_1 {
         String value1="hi this is a String";
         System.out.println(value);
         System.out.println(value1);
+
+        //We must not have any characters when converting from String to the numerical values
+        int number=Integer.parseInt(value); //this function converts the String number into integer
+        System.out.println(number);
+
+        String doubval="90.0";
+        double number2=Double.parseDouble(doubval);
+        System.out.println(number2);
+    
+        
+        String floval="90.0"; //Here we have a staring literal f inside but it will not show error because float has ending value of f.
+        float number3=Float.parseFloat(floval);
+        System.out.println(number3);
     }
     
 }
