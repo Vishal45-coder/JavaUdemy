4597053779668e91326d05e8bedd6365cd26ff8f
Vishal45-coder <vishalraavi1999@gmail.com>
2021-07-21 21:48:18 +0530

    Added code and comments

--- src/Getter_setter.java
+++ src/Getter_setter.java
@@ -4,8 +4,12 @@ public class Getter_setter {
 	public static void main(String[] args) {
 		
 		ge h1=new ge();
-        h1.setA(10);
-		System.out.println(h1.getA());
+
+		h1.setB("Vishal"); //IT sets the b value of Vishal for object h1
+		System.out.println("Name : "+h1.getB()); //THis will get the value of b for the object  h1
+
+		h1.setA(10); //IT sets the a value of 10 for object h1
+		System.out.println("Age : "+h1.getA()); //THis will get the value of a for the object h1
 	}
 
 }
