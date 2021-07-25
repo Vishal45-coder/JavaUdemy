fb5ebcee64175bf3a96d1489dca4bcb109dea730
Vishal45-coder <vishalraavi1999@gmail.com>
2021-07-25 16:52:58 +0530

    Added printwithListIterator method in Demo.java

--- src/Linked_lists/Demo.java
+++ src/Linked_lists/Demo.java
@@ -4,13 +4,15 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 public class Demo {
     private LinkedList CreatingLinkedLst() {
         LinkedList<String> placesToVisit = new LinkedList<String>();
         return placesToVisit;
     }
-    private void AddingElement(LinkedList<String> VisitPlaces){
+
+    private void AddingElement(LinkedList<String> VisitPlaces) {
         VisitPlaces.add("Sydney");
         VisitPlaces.add("Melbourne");
         VisitPlaces.add("Brisbane");
@@ -20,34 +22,48 @@ public class Demo {
         VisitPlaces.add("Darwin");
     }
 
-    private void InsertingElement(LinkedList<String> VisitPlaces){
-        VisitPlaces.add(1,"Alice Springs");
+    private void InsertingElement(LinkedList<String> VisitPlaces) {
+        VisitPlaces.add(1, "Alice Springs");
     }
 
-    private void removingElement(LinkedList<String> Visitplaces){
+    private void removingElement(LinkedList<String> Visitplaces) {
         Visitplaces.remove(3);
     }
 
     //We have used Iterator class to print Elements
-    private void printList(LinkedList<String> placesList){
-        Iterator<String> i=placesList.iterator(); //To create an iterator object
-        while (i.hasNext()){ //To check there is an element in the object  iterator
-            System.out.println("Now visiting "+i.next()); //to print the element in the object iterator
+    private void printwithIterator(LinkedList<String> VisitPlaces) {
+        Iterator<String> i = VisitPlaces.iterator(); //To create an iterator object
+        while (i.hasNext()) { //To check there is an element in the object  iterator
+            System.out.println("Now visiting " + i.next()); //to print the element in the object iterator
         }
         System.out.println();
     }
-    public static void main(String[] args) {
-        Demo List1=new Demo();
-        LinkedList<String> places= List1.CreatingLinkedLst();
 
+    private void printwithListIterator(LinkedList VisitPlaces) {
+        ListIterator<String> it = VisitPlaces.listIterator();
+        while (it.hasNext()) {
+            System.out.println("printing present element " + it.next());
+        }
+        System.out.println();
+        //This method is only available in LinkedList.
+        //It is used when we want to traverse the list from end
+        while (it.hasPrevious()) {
+            System.out.println("printing previous element " + it.previous());
+        }
+    }
+
+    public static void main(String[] args) {
+        Demo List1 = new Demo();
+        LinkedList<String> places = List1.CreatingLinkedLst();
         List1.AddingElement(places);
-        List1.printList(places);
+        List1.printwithIterator(places);
 
         List1.InsertingElement(places);
-        List1.printList(places);
+        List1.printwithIterator(places);
 
         List1.removingElement(places);
-        List1.printList(places);
+        List1.printwithIterator(places);
 
+        List1.printwithListIterator(places);
     }
 }
