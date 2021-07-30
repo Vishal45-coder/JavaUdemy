0837f138b0c81f15bb9ffe9f82a6cd136b384756
Vishal45-coder <vishalraavi1999@gmail.com>
2021-07-30 08:01:54 +0530

    Created a program to check if an element is present in the array

---
+++ src/Arrays_/Array_Element_check.java
@@ -0,0 +1,11 @@
+package Arrays_;
+
+import java.util.Arrays;
+
+public class Array_Element_check {
+    public static void main(String[] args) {
+        int[] A = new int[]{1,2,3,4,5,6};
+        String element="5";
+        System.out.println(Arrays.toString(A).contains(element));
+    }
+}
